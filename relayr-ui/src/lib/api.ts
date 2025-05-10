8e204ad4aed462aa3418b7d4a82428fd6c3acf9f
rifuki <rifuki.dev@gmail.com>
2025-05-11 00:42:00 +0700

    test deploy change http to https

--- relayr-ui/src/lib/api.ts
+++ relayr-ui/src/lib/api.ts
@@ -1,6 +1,6 @@
 const BASE_API_URL = `${process.env.NEXT_PUBLIC_API_SOCKET_ADDRESS}/api/v1`;
 
-const RELAY_API_URL = `http://${BASE_API_URL}/relay`;
-const WS_RELAY_API_URL = `ws://${BASE_API_URL}/relay`;
+const RELAY_API_URL = `https://${BASE_API_URL}/relay`;
+const WS_RELAY_API_URL = `wss://${BASE_API_URL}/relay`;
 
 export { BASE_API_URL, RELAY_API_URL, WS_RELAY_API_URL };
